0cb0b6e12b1e5032417a0a244e7b279206ae893d
joyanlj <joyanlj@4c19c623-604c-0410-8048-e75c7dd52f95>
2008-05-18 03:58:14 +0000

    加入了WordNet的介绍。 加入了WordPool的草稿版。

---
+++ trunk/src/wordpool.java
@@ -0,0 +1,29 @@
+//----------------------------------------------------------------------
+// FileName: wordpool.java
+// Author: Chen Zhijie
+// Contents: A data pool contains all the elements in the wordnet.
+//----------------------------------------------------------------------
+
+class WordPool
+{
+    WordPool();
+
+    String GetSynonymy(String word,char attr);//nvad
+    String GetAntonym(String word,char attr);//nvad
+    String GetHyponym(String word,char attr);//n
+    String GetHypenym(String word,char attr);//nv
+    String GetMeronym(String word,char attr);//n
+    String GetHolonym(String word,char attr);//n
+    String GetAttribute(String word,char attr);//na
+
+    String GetTroponym(String word,char attr);//v
+    String GetEntailment(String word,char attr);//v
+    String GetCause(String word,char attr);//v
+    String GetAlsoSee(String word,char attr);//va
+
+    String GetSimilar(String word,char attr);//a
+    String GetRelational(String word,char attr);//a
+
+    String GetDerivedFrom(String word,char attr);//d
+    
+}
