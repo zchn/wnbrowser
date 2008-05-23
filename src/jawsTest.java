71a5d6fd927cf9e0339e19d5c2725f014300868a
joyanlj <joyanlj@4c19c623-604c-0410-8048-e75c7dd52f95>
2008-05-23 05:09:12 +0000

    增强了测试文件的功能，现在运行的时候不需要用－D选项传入参数了。 附Linux下编译运行方法：
    编译：
    make
    (windows下编译大概是
    javac -cp "./lib/jaws.jar" jawsTest.java)
    
    运行：
    java -cp $CLASSPATH:./lib/jaws.jar jawsTest
    
    注意classpath的设置。
    
    建议不要把.class文件传入svn库（加入ignorelist即可）
    
    另外上次传的那两个文档（subversion的和注释的）是我随便传的项目外读物，
    忙的话不需要读～～呵呵

--- src/jawsTest.java
+++ src/jawsTest.java
@@ -1,4 +1,5 @@
 import edu.smu.tspell.wordnet.*;
+import java.io.File;
 //import edu.smu.tspell.wordnet.Synset;
 //import edu.smu.tspell.wordnet.WordNetDatabase;
 //import edu.smu.tspell.wordnet.SynsetType;
@@ -7,13 +8,9 @@ public class jawsTest{
     private static final String WORDNET_DATABASE_DIR_TAG = "wordnet.database.dir";
     public static void main(String[] args) 
     {
-        // System.setProperty(WORDNET_DATABASE_DIR_TAG, System
-        //                    .getProperty("user.dir")
-        //                    + File.separator
-        //                    + "lib"
-        //                    + File.separator
-        //                    + "en"
-        //                    + File.separator + "dict");
+        System.setProperty(WORDNET_DATABASE_DIR_TAG,
+                            "."
+                            + File.separator + "dict");
         WordNetDatabase database = WordNetDatabase.getFileInstance();
         
         Synset[] nounFlies = database.getSynsets("fly", SynsetType.NOUN);
