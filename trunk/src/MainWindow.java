8d18b7c1028c6fd4d6c8a8a64fd7dfbc46bdceb2
joyanlj <joyanlj@4c19c623-604c-0410-8048-e75c7dd52f95>
2008-06-10 15:39:58 +0000

    将jaws库（WordNetDB库接口）加入到应用程序中，目前已经初步可以使用了～～^_^

--- trunk/src/MainWindow.java
+++ trunk/src/MainWindow.java
@@ -1,12 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import org.jgraph.*;
 import org.jgrapht.*;
-import java.awt.*;
+
 
 import java.awt.geom.Rectangle2D;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+
+import edu.smu.tspell.wordnet.*;
 
 import org.jgraph.graph.DefaultGraphCell;
 import org.jgraph.graph.GraphConstants;
@@ -16,29 +21,96 @@ import org.jgrapht.ext.JGraphModelAdapter;
 import org.jgrapht.graph.ListenableDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 
+
 public class MainWindow extends JFrame
 {
-    int WORDWIDTH=30;
+    private static final int WORDWIDTH=30;
+    private static final String WORDNET_DATABASE_DIR_TAG = "wordnet.database.dir";
     
     JTextField txtWord;
     JButton btnSearch,btnNoun,btnVerb,btnAdj,btnAdv;
     JList lstMeanings,lstRelatedWords;
     JGraph grpWordNet;
     JTextArea txaMeaning;
-    
+    WordNetDatabase dbWordNet;
+    String currWord;
+    Synset[] currSynset;
+    SynsetType currProp;
+    int currMeaningIdx;
+
     public MainWindow()
     {
-
+        InitWordnetDB();
         InitWindowFrame();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
         pack();
         validate();
         setVisible(true);
     }
-            
-        
-    private void InitWindowFrame()
+
+    
+    private void InitWordnetDB() 
     {
+        System.setProperty(WORDNET_DATABASE_DIR_TAG,
+                            "."
+                            + File.separator + "dict");
+        dbWordNet = WordNetDatabase.getFileInstance();
+    }
+
+    private class SearchBtnHandler implements ActionListener
+    {
+        public void actionPerformed( ActionEvent e)
+        {
+            currWord = txtWord.getText();
+            txaMeaning.setText(currWord);
+            txaMeaning.validate();
+            currSynset = dbWordNet.getSynsets(currWord, SynsetType.NOUN);
+            currProp = SynsetType.NOUN;
+            currMeaningIdx = 0;
+            UpdateMeanings();
+            UpdateRelatedWords();
+            UpdateMeaning();            
+        }
+    }
+
+
+    private class PropBtnHandler implements ActionListener
+    {
+        public void actionPerformed( ActionEvent e)
+        {
+            
+        }
+    }
+
+    
+    private void UpdateMeanings()
+    {
+        int i;
+        String[] meaningsList = new String[currSynset.length];
+        for(i = 0; i < currSynset.length; i++){
+            meaningsList[i] = (currSynset[i].getDefinition());
+        }
+        lstMeanings.setListData(meaningsList);
+        lstMeanings.validate();
+    }
+
+    private void UpdateRelatedWords()
+    {
+        String[] relatedWordList;
+        relatedWordList = currSynset[currMeaningIdx].getWordForms();
+        lstRelatedWords.setListData(relatedWordList);
+        lstRelatedWords.validate();
+    }
+    private void UpdateMeaning()
+    {
+        txaMeaning.setText(currSynset[currMeaningIdx].getDefinition());
+        txaMeaning.validate();
+    }
+
+    private void InitWindowFrame()
+        
+    {
+        
         txtWord = new JTextField(WORDWIDTH);
         
         btnSearch = new JButton("Search");
@@ -49,6 +121,7 @@ public class MainWindow extends JFrame
 
         String[] tmp1 = {"Here","Shows","the","meanings","^_^"};
         String[] tmp2 = {"Here","Shows","related","words","^_^"};
+        btnSearch.addActionListener(new SearchBtnHandler());
         lstMeanings = new JList(tmp1);
         lstRelatedWords = new JList(tmp2);
         grpWordNet = new JGraph();
@@ -67,22 +140,30 @@ public class MainWindow extends JFrame
         
         leftSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
                                    new JScrollPane(lstMeanings),
-                                   new JScrollPane(lstRelatedWords));                                   
+                                   new JScrollPane(lstRelatedWords));
+        leftSplit.setDividerLocation(0.5);
         rightSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
                                      new JScrollPane(grpWordNet),
                                      new JScrollPane(txaMeaning));
+        rightSplit.setDividerLocation(0.8);        
+
         downSplit = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
                                    leftSplit,
                                    rightSplit);
         upSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
                                  pnlInput,
                                  pnlProp);
+        upSplit.setDividerSize(0);
+        downSplit.setDividerLocation(0.4);
+        
         baseSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
                                    upSplit,
                                    downSplit);
+        baseSplit.setDividerSize(0);
         getContentPane().add(baseSplit);
         
         
         
     }
+    
 }       
