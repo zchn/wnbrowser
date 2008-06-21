2130da489b4d86cb902368fe905c858c7822050a
joyanlj <joyanlj@4c19c623-604c-0410-8048-e75c7dd52f95>
2008-06-21 12:31:12 +0000

    添加了FileDatabase的公共函数。

--- src/lib/edu/pku/ymd/wordnet/FileDatabase.java
+++ src/lib/edu/pku/ymd/wordnet/FileDatabase.java
@@ -2,4 +2,7 @@ package edu.pku.ymd.wordnet;
 
 public class FileDatabase
 {
+    public static FileDatabase getInstance(String dictPath);
+    public Synset[] getSynset(String WordForm);
+    public Synset[] getSynset(int offset);
 }
