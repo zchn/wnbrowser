e6ab6ce3c0c6c8bd3f8462fbb81aab4105de5756
joyanlj <joyanlj@4c19c623-604c-0410-8048-e75c7dd52f95>
2008-06-22 12:29:46 +0000

    完成了Synset中的Nounsynset和NounWordset。 部分完成从InnerSynsets到Synset的转化。

--- trunk/src/lib/edu/pku/ymd/wordnet/NounWordset.java
+++ trunk/src/lib/edu/pku/ymd/wordnet/NounWordset.java
@@ -2,13 +2,46 @@ package edu.pku.ymd.wordnet;
 
 class NounWordset extends Wordset implements NounSynset
 {
+    WordPair[] Antonyms;
+    WordPair[] Attributes;
+    WordPair[] Hypernyms;
+    WordPair[] Hyponyms;
     
-    WordSense[] getAntonyms(String wordForm);
+    String[] getAntonyms(String wordForm)
+    {
+        String[] ret = new String[Antonyms.length()];
+        for(int i = 0; i < Antonyms.length(); i++){
+            ret[i] = Antonyms[i].word;
+        }
+        return ret;
+    }
     //Returns the antonyms (words with the opposite meaning), if any, associated with a word form in this synset.
-    AdjectiveSynset[] getAttributes();
+    String[] getAttributes()        
+    {
+        String[] ret = new String[Attributes.length()];
+        for(int i = 0; i < Attributes.length(); i++){
+            ret[i] = Attributes[i].word;
+        }
+        return ret;
+    }
     //Returns adjectives that describes states associated with this noun synset's concept.
-    NounSynset[] getHypernyms();
+    String[] getHypernyms()
+    {
+        String[] ret = new String[Hypernyms.length()];
+        for(int i = 0; i < Hypernyms.length(); i++){
+            ret[i] = Hypernyms[i].word;
+        }
+        return ret;
+    }
+        
     //Returns the direct hypernyms (superordinate types), if any, of this type.
-    NounSynset[] getHyponyms();
+    String[] getHyponyms()    
+    {
+        String[] ret = new String[Hyponyms.length()];
+        for(int i = 0; i < Hyponyms.length(); i++){
+            ret[i] = Hyponyms[i].word;
+        }
+        return ret;
+    }
     //Returns the direct hyponyms (subordinate type), if any, of this type.
 }
--- trunk/src/lib/edu/pku/ymd/wordnet/WordNetDatabase.java
+++ trunk/src/lib/edu/pku/ymd/wordnet/WordNetDatabase.java
@@ -2,25 +2,100 @@ package edu.pku.ymd.wordnet;
 
 public class WordNetDatabase
 {
-    WordNetDatabase instance;
+    static WordNetDatabase instance;
+
+    ptivate int synsetCacheSize = 500;
+    ptivate int offsetCacheSize = 500;    
+
+    private Map<String,Synset[]> synsetCache;
+    private Map<String,int> offsetCache;
     
-    private WordNetDataBase(){}
-    static WordNetDatabase getFileInstance();
+    private WordNetDataBase(){
+        synsetCache = new HashMap(synsetCacheSize);
+        offsetCache = new HashMap(offsetCacheSize);
+    }
+    static WordNetDatabase getFileInstance()
+    {
+        if(instance == null){
+            instance = new WordNetDatabase();
+            return instance;
+        }else{
+            return instance;
+        }
+    }
     //Returns an implementation of this class that can access
     //the WordNet database by searching files on the local file system.
-    Synset[] getSynsets(String wordForm);
+    Synset[] getSynsets(String wordForm)
+    {
+        if(synsetCache.containsKey(wordForm)){
+            return synsetCache.get(wordForm);
+        }else{
+            FileDatabase filedb = FileDatabase.getInstance();
+            InnerSynset[] result;
+            if(offsetCache.containsKey(wordForm)){
+                result = filedb.getSynsets(offsetCache.get(wordForm));
+            }else{
+                result = filedb.getSynsets(wordForm);
+            }
+            Synset[] resSynsets = ConvertInnerToOuter(result);
+            AddToCaches(resSynsets);
+            return resSynsets;
+        }
+    }
+    
     Synset[] getSynsets(String wordForm, SynsetType type)
     {
         Synset[] result = getSynsets(wordForm);
         int i;
-        int cnt = 0;
+        List<Synset> ret = new ArrayList<Synset>;
         for(i = 0; i < result.length(); i++){
             if(result[i].getType() == type){
-                cnt++;
+                ret.add(result[i]);
             }
         }
+        Synset[] retArray = new Wordset[ret.size()];
+        ret.toArray(retArray);
+        return retArray;
     }
 
-    Synset[] getSynsets(String wordForm, SynsetType type, boolean useMorphology);
+    Synset[] getSynsets(String wordForm, SynsetType type, boolean useMorphology)
+    {
+        return getSynsets(wordForm,type);
+    }
+
+    
+    Synset[] ConvertInnerToOuter(InnerSynset[] result)
+    {
+        int i;
+        List<Synset> retList = new ArrayList<Synset>;
+        for(i = 0; i < result.length(); i++){
+            switch (result[i].getType()){
+            case SynsetType.NOUN:
+                retList.add(ItoONoun((InnerNounSynset)result[i]));
+                break;
+                
+            case SynsetType.VERB:
+                retList.add(ItoOVerb((InnerVerbSynset)result[i]));
+                break;
+                
+            case SynsetType.ADVERB:
+                retList.add(ItoOAdverb((InnerAdverbSynset)result[i]));
+                break;
+                
+            case SynsetType.ADJECTIVE:
+                retList.add(ItoOAdjective((InnerAdjectiveSynset)result[i]));
+                break;
+            }
+        }
+        return ((Synset[])retList.toArray());
+    }
+    
+    NounSynset ItoONoun(InnerNounSynset result)
+    {
+        NounWordset ret = new NounWordset();
         
+    }
+
+
+    void AddToCaches(Synset[] resSynsets);
 }
