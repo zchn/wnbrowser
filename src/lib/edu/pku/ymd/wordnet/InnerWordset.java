27a6c1c476d5f6916c57280877a3b79aac8e0d90
ywq8876@163.com <ywq8876@163.com@4c19c623-604c-0410-8048-e75c7dd52f95>
2008-06-22 08:13:38 +0000

    内部数据结构更新2

--- src/lib/edu/pku/ymd/wordnet/InnerWordset.java
+++ src/lib/edu/pku/ymd/wordnet/InnerWordset.java
@@ -1,3 +1,30 @@
 package edu.pku.ymd.wordnet
 
 class InnerWordset
+{
+    WordPair centerWord;
+    String definition;
+    SynsetType type;
+    String[] usageExample;
+    WordPair[] wordForms;
+
+    String getDefinition()
+    {
+        return definition;
+    }
+    //Retrieve a short description / definition of this concept.
+    SynsetType getType()
+    {
+        return type;
+    }
+    //Retrieve the type of synset this object represents.
+    String[] getUsageExamples()
+    {
+        return usageExample;
+    }
+        //Retrieve sentences showing examples of how this synset is used.
+    WordPair[] getWordForms()
+    {
+        return wordForms;
+    }    
+}
